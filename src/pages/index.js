cd5e00c0722feeb1b69cecd3afb5681be4b138fe
Rufus31415 <rufus31415@gmail.com>
2023-07-06 13:51:15 +0200

    suppression de la timeline, de l'animation et du texte en bas à droite du globe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -6,15 +6,12 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 import { Viewer, GeoJsonDataSource } from "resium";
-import { Color } from "cesium";
-/*
-Cesium.Ion.defaultAccessToken =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMmMxODRjYy1mYzFiLTQ5MTUtODE1MS02NGNkMzAyNTIyODciLCJpZCI6MTA2OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJhc3NldHMiOlsyLDMsNCwxXSwiaWF0IjoxNTU3MjA1NTM1fQ.5TYPEJKj_JzGX4r_a6GQjwSu7TIW2BIzeaIW8gFLUec";
-*/
+import { Color, Ion } from "cesium";
+
+Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMmMxODRjYy1mYzFiLTQ5MTUtODE1MS02NGNkMzAyNTIyODciLCJpZCI6MTA2OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJhc3NldHMiOlsyLDMsNCwxXSwiaWF0IjoxNTU3MjA1NTM1fQ.5TYPEJKj_JzGX4r_a6GQjwSu7TIW2BIzeaIW8gFLUec";
+
 
 const urlConsigneParcours = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/ConsigneParcours!A:H/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
-
-
 
 
 
@@ -44,9 +41,7 @@ export default function Home() {
   const [consigneParcoursData, setConsigneParcoursData] = useState(null);
 
   useEffect(() => {
-    fetch(urlConsigneParcours).then(x=>x.json()).then(x=>{
-      console.log(x)
-
+    fetch(urlConsigneParcours).then(x => x.json()).then(x => {
       let data = {
         "type": "FeatureCollection",
         "features": [
@@ -61,8 +56,8 @@ export default function Home() {
         ]
       }
 
-      for(let i=1;i<x.values.length;i++){
-        let coordinates = [parseFloat(x.values[i][4]),  parseFloat(x.values[i][5]) ]
+      for (let i = 1; i < x.values.length; i++) {
+        let coordinates = [parseFloat(x.values[i][4]), parseFloat(x.values[i][5])]
 
         data.features[0].geometry.coordinates.push(coordinates) // ajout des points à la ligne
 
@@ -95,7 +90,7 @@ export default function Home() {
           <div className="row">
             <div className={clsx('col')}>
               <div className="text--center">
-                <Viewer >
+                <Viewer timeline={false} animation={false} >
                   <GeoJsonDataSource
                     data={consigneParcoursData}
                     markerColor={Color.RED}
