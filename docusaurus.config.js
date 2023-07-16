59408a7c2648eb6b5abc7511a6d7b593f5377d26
Eric Boucher <eric.p.boucher@gmail.com>
2023-07-16 13:15:26 +0200

    missing link to intro in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,15 +92,15 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
           {
             title: 'Community',
             items: [
