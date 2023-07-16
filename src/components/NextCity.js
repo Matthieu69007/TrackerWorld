4eeaf5a7b47e9f5bbc253818385970d5d2f687d9
Eric Boucher <eric.p.boucher@gmail.com>
2023-07-16 13:10:20 +0200

    split previous and next data into two sets

--- src/components/NextCity.js
+++ src/components/NextCity.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import React, { useState, useEffect } from "react"
 
-const urlNextcity = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/TraceReelle!B:D/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
+export const urlNextcity = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/TraceReelle!B:F/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
 
 
 export default function NextCity() {
--- src/components/ViewerComponent.js
+++ src/components/ViewerComponent.js
@@ -1,19 +1,46 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Entity, Viewer, GeoJsonDataSource } from "resium";
 import { Cartesian2, Cartesian3, Color } from "cesium";
+import { urlNextcity } from "./NextCity";
 
 const urlConsigneParcours =
   "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/ConsigneParcours!A:H/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68";
 
 export default function ViewerComponent() {
-  const [consigneParcoursData, setConsigneParcoursData] = useState(null);
+  const [consigneParcoursPreviousData, setConsigneParcoursPreviousData] =
+    useState(null);
+  const [consigneParcoursNextData, setConsigneParcoursNextData] =
+    useState(null);
+  const [nextCityId, setNextCityId] = useState(1);
+
   const viewerRef = useRef(null);
 
-  useEffect(() => {
+  useEffect(async () => {
+    await fetch(urlNextcity)
+      .then((x) => x.json())
+      .then((x) => {
+        const cityId = (x?.values && parseFloat(x.values[1][3])) || 1;
+        setNextCityId(cityId);
+      });
+
     fetch(urlConsigneParcours)
       .then((x) => x.json())
       .then((x) => {
-        let data = {
+        let previousData = {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: [],
+              },
+            },
+          ],
+        };
+
+        let nextData = {
           type: "FeatureCollection",
           features: [
             {
@@ -33,27 +60,36 @@ export default function ViewerComponent() {
             parseFloat(x.values[i][5]),
           ];
 
-          data.features[0].geometry.coordinates.push(coordinates); // ajout des points à la ligne
+          const properties = {
+            city: x.values[i][1],
+            name: x.values[i][6],
+          };
 
-          // ajout des étiquettes
-          data.features.push({
+          const pointData = {
             type: "Feature",
-            properties: {
-              city: x.values[i][1],
-              name: x.values[i][6],
-            },
+            properties,
             geometry: {
               type: "Point",
               coordinates: coordinates,
             },
-          });
+          };
+
+          const cityId = parseFloat(x.values[i][0]);
+
+          if (cityId <= nextCityId) {
+            previousData.features[0].geometry.coordinates.push(coordinates); // ajout des points à la ligne
+            previousData.features.push(pointData);
+          }
+          if (cityId >= nextCityId) {
+            nextData.features[0].geometry.coordinates.push(coordinates); // ajout des points à la ligne
+            // ajout des étiquettes
+            nextData.features.push(pointData);
+          }
         }
-
-        setConsigneParcoursData(data);
+        setConsigneParcoursPreviousData(previousData);
+        setConsigneParcoursNextData(nextData);
       });
-  }, []);
-
-  console.log(consigneParcoursData);
+  }, [nextCityId]);
 
   const enterFullscreen = () => {
     if (viewerRef.current) {
@@ -78,35 +114,44 @@ export default function ViewerComponent() {
       <Viewer ref={viewerRef} timeline={false} animation={false}>
         <GeoJsonDataSource
           markerSymbol=""
-          data={consigneParcoursData}
+          data={consigneParcoursPreviousData}
+          stroke={Color.GREEN}
           // hide markers
           markerColor={Color.TRANSPARENT}
         />
-        {/* Loop through consigneParcoursData and create point enitities */}
-        {consigneParcoursData &&
-          consigneParcoursData.features.map((feature, index) => {
-            console.log(feature);
-            if (feature.geometry.type === "Point") {
-              const [longitude, latitude] = feature.geometry.coordinates;
-              const city = feature.properties.city;
-              const name = feature.properties.name;
-              return (
-                <Entity
-                  description={name}
-                  name={city}
-                  point={{ pixelSize: 10 }}
-                  label={{
-                    text: city,
-                    font: "14pt monospace",
-                    pixelOffset: new Cartesian2(0, -20),
-                  }}
-                  // remove the on click handler
-                  onClick={() => {}}
-                  position={Cartesian3.fromDegrees(longitude, latitude, 100)}
-                />
-              );
-            }
-          })}
+        <GeoJsonDataSource
+          markerSymbol=""
+          data={consigneParcoursNextData}
+          stroke={Color.RED}
+          // hide markers
+          markerColor={Color.TRANSPARENT}
+        />
+        {/* Loop through consigneParcoursData and create point entities */}
+        {[
+          ...(consigneParcoursPreviousData?.features || []),
+          ...(consigneParcoursNextData?.features || []),
+        ].map((feature) => {
+          if (feature.geometry.type === "Point") {
+            const [longitude, latitude] = feature.geometry.coordinates;
+            const city = feature.properties.city;
+            const name = feature.properties.name;
+            return (
+              <Entity
+                description={name}
+                name={city}
+                point={{ pixelSize: 10 }}
+                label={{
+                  text: city,
+                  font: "14pt",
+                  pixelOffset: new Cartesian2(0, -20),
+                }}
+                // remove the on click handler
+                onClick={() => {}}
+                position={Cartesian3.fromDegrees(longitude, latitude, 100)}
+              />
+            );
+          }
+        })}
       </Viewer>
     </div>
   );
