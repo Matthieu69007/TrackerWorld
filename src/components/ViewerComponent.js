fbbee94aa9eda9a424fba3fd0dd6605d1b282c7b
Eric Boucher <eric.p.boucher@gmail.com>
2023-07-16 12:18:33 +0200

    add floating city names

--- src/components/ViewerComponent.js
+++ src/components/ViewerComponent.js
@@ -1,50 +1,59 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Viewer, GeoJsonDataSource } from "resium";
-import { Color } from "cesium";
+import React, { useEffect, useState, useRef } from "react";
+import { Entity, Viewer, GeoJsonDataSource } from "resium";
+import { Cartesian2, Cartesian3, Color } from "cesium";
 
-const urlConsigneParcours = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/ConsigneParcours!A:H/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
+const urlConsigneParcours =
+  "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/ConsigneParcours!A:H/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68";
 
 export default function ViewerComponent() {
   const [consigneParcoursData, setConsigneParcoursData] = useState(null);
   const viewerRef = useRef(null);
 
   useEffect(() => {
-    fetch(urlConsigneParcours).then(x => x.json()).then(x => {
-      let data = {
-        "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-              "type": "LineString",
-              "coordinates": []
-            }
-          }
-        ]
-      }
+    fetch(urlConsigneParcours)
+      .then((x) => x.json())
+      .then((x) => {
+        let data = {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: [],
+              },
+            },
+          ],
+        };
 
-      for (let i = 1; i < x.values.length; i++) {
-        let coordinates = [parseFloat(x.values[i][4]), parseFloat(x.values[i][5])]
+        for (let i = 1; i < x.values.length; i++) {
+          let coordinates = [
+            parseFloat(x.values[i][4]),
+            parseFloat(x.values[i][5]),
+          ];
 
-        data.features[0].geometry.coordinates.push(coordinates) // ajout des points à la ligne
+          data.features[0].geometry.coordinates.push(coordinates); // ajout des points à la ligne
 
-        // ajout des étiquettes
-        data.features.push({
-          "type": "Feature",
-          "properties": {
-            "name": x.values[i][6]
-          },
-          "geometry": {
-            "type": "Point",
-            "coordinates": coordinates
-          }
-        })
-      }
+          // ajout des étiquettes
+          data.features.push({
+            type: "Feature",
+            properties: {
+              city: x.values[i][1],
+              name: x.values[i][6],
+            },
+            geometry: {
+              type: "Point",
+              coordinates: coordinates,
+            },
+          });
+        }
 
-      setConsigneParcoursData(data)
-    })
+        setConsigneParcoursData(data);
+      });
   }, []);
+
+  console.log(consigneParcoursData);
 
   const enterFullscreen = () => {
     if (viewerRef.current) {
@@ -64,13 +73,40 @@ export default function ViewerComponent() {
   };
 
   return (
-    <div style={{ height: '40vh' }}>
+    <div>
       <button onClick={enterFullscreen}>Plein écran</button>
       <Viewer ref={viewerRef} timeline={false} animation={false}>
         <GeoJsonDataSource
+          markerSymbol=""
           data={consigneParcoursData}
-          markerColor={Color.Blue}
+          // hide markers
+          markerColor={Color.TRANSPARENT}
         />
+        {/* Loop through consigneParcoursData and create point enitities */}
+        {consigneParcoursData &&
+          consigneParcoursData.features.map((feature, index) => {
+            console.log(feature);
+            if (feature.geometry.type === "Point") {
+              const [longitude, latitude] = feature.geometry.coordinates;
+              const city = feature.properties.city;
+              const name = feature.properties.name;
+              return (
+                <Entity
+                  description={name}
+                  name={city}
+                  point={{ pixelSize: 10 }}
+                  label={{
+                    text: city,
+                    font: "14pt monospace",
+                    pixelOffset: new Cartesian2(0, -20),
+                  }}
+                  // remove the on click handler
+                  onClick={() => {}}
+                  position={Cartesian3.fromDegrees(longitude, latitude, 100)}
+                />
+              );
+            }
+          })}
       </Viewer>
     </div>
   );
