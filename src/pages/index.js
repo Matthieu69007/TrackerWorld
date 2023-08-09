e0c014329a3418a3c4b06bf464c3749e6839c6e5
thibaultgrad <thibault.grad@gmail.com>
2023-08-09 22:36:18 +0200

    Ajout de texte en gras dans les regles suppression de la timeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ import { Color, Ion } from "cesium";
 import ViewerComponent from '../components/ViewerComponent';
 import StylesForLinkText from './index.module.css';
 import CustomizedTimeline from '../components/TimeLine';
+import { Typography } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
 
 Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMmMxODRjYy1mYzFiLTQ5MTUtODE1MS02NGNkMzAyNTIyODciLCJpZCI6MTA2OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJhc3NldHMiOlsyLDMsNCwxXSwiaWF0IjoxNTU3MjA1NTM1fQ.5TYPEJKj_JzGX4r_a6GQjwSu7TIW2BIzeaIW8gFLUec";
 
@@ -52,7 +55,7 @@ export default function Home() {
           <div className="row">
             <div className={clsx('col')}>
               <div className="text--center">
-                <br /> 
+                <br />
                 <CustomizedTimeline />
                 <Link
                   className={`button button--secondary button--lg ${styles.customLinkText}`}
@@ -61,6 +64,12 @@ export default function Home() {
                 </Link>
                 <br /><br /> <br />
                 <HomepageFeatures />
+                <br />
+                  <Chip 
+                  label="Carte intercative du chemin parcouru et à parcourir par trace"
+                  color="success"
+                  size="medium"
+                  />
                 <br /><br />
                 <ViewerComponent />
                 <br /> <br />
@@ -68,7 +77,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        
+
       </main>
     </Layout>
   );
