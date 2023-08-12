b17750c9778edd10ffb90faaac5410284cc0b8b8
sbsrouteur <11095871+sbsrouteur@users.noreply.github.com>
2023-08-12 21:23:06 +0200

    Fixer le pied de page fix #29

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © 2023- TraceAcrossTheWorld.com. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
