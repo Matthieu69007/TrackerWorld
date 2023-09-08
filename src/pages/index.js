99dbff1058d9fef9d9deba04d8a25abedab101b2
sbs <>
2023-09-08 21:43:19 +0200

    Add Translate tags for index translation.

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ import ViewerComponent from '../components/ViewerComponent';
 import Typography from '@mui/material/Typography';
 import Rules from '../../docs/Rules.mdx';
 import Itinerary from '../../docs/Itinerary.mdx';
+import Translate from '@docusaurus/Translate';
 
 Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMmMxODRjYy1mYzFiLTQ5MTUtODE1MS02NGNkMzAyNTIyODciLCJpZCI6MTA2OTgsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJhc3NldHMiOlsyLDMsNCwxXSwiaWF0IjoxNTU3MjA1NTM1fQ.5TYPEJKj_JzGX4r_a6GQjwSu7TIW2BIzeaIW8gFLUec";
 
@@ -22,8 +23,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title"><Translate desc='Trace across the world'> Trace across the world</Translate></h1>
+        <p className="hero__subtitle"> <Translate desc='Le tour du monde depuis chez soi !'>Le tour du monde depuis chez soi !</Translate></p>
         <div className={styles.buttons}>
           
         </div>
@@ -34,7 +35,7 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
-
+  const ChipMap = <Translate desc="Carte intercative du chemin parcouru et à parcourir par trace">Carte intercative du chemin parcouru et à parcourir par trace</Translate>
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -45,11 +46,11 @@ export default function Home() {
           <div className="row">
             <div className={clsx('col')}>
               <div className="text--center">
-              <Typography fontSize={50}>Le projet en quelques mots</Typography>
+              <Typography fontSize={50}><Translate desc='Le projet en quelques mots'>Le projet en quelques mots</Translate></Typography>
                <HomepageFeatures />
                 <br />
                   <Chip 
-                  label="Carte intercative du chemin parcouru et à parcourir par trace"
+                  label={ChipMap}
                   color="success"
                   size="medium"
                   />
