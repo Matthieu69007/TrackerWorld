0008f5ea23f5e636cc64553192ec183a0224811d
sbs <>
2023-10-29 08:09:29 +0100

    remove react-fontawesome packages.

--- src/components/Map/DialogPicture.js
+++ src/components/Map/DialogPicture.js
@@ -33,22 +33,36 @@ export default function DialogPicture(Props)
         DialogImage.destroy()
       }
       d = L.control.dialog({
-            size:[X,Y],
+            size:[0,0],
+            maxSize:[screen.width,screen.height],
+            minSize:[100,100],
             anchor:[YOffset,XOffset],
             position:'topleft',
             initOpen:true
           }).addTo(Map).close()
       SetDialogImage( d)      
     }
+    else if (false && X!==DialogImage.options.size[0] )
+    {
+
+      DialogImage.setSize([X,Y]).setLocation([YOffset,XOffset])
+      console.log("resized to ",X,Y, MapIsFullScreen, DialogImage.options.size[0],DialogImage.options.size[1])
+      
+    }
+    
     if (MapIsFullScreen && DialogImage && Props?.URL)
       {
-        console.log(Props.URL)
+        console.log("opening FSDialog", Props.URL)
         DialogImage.setContent("<Box sx={style}><img src='"+Props.URL+"' width='100%'/></Box>")
         DialogImage.open().unfreeze()
       }
-      else if (!MapIsFullScreen && DialogImage)
+      else if (!MapIsFullScreen && DialogImage && DialogImage.is)
       {
+        
         DialogImage.close()
+        DialogImage.destroy()
+        SetDialogImage(null)
+        if (ShowDialogCallback) ShowDialogCallback(null)
       }
       else
       {
