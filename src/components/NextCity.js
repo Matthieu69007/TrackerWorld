1cc85ecfe19dd832e1e8cdf79049d355eaf119f7
sbs <>
2024-07-18 22:43:15 +0200

    Fix url in rules mdx

--- src/components/NextCity.js
+++ src/components/NextCity.js
@@ -5,12 +5,12 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import React, { useState, useEffect } from "react"
 
-export const urlNextcity = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/TraceReelle!B:G/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
+export const urlNextcity = "https://sheets.googleapis.com/v4/spreadsheets/1FNX9RpTH7WgQKxqpfvGJ7koBMNxcFUtTRvzAIoD8iyI/values/TraceReelle!B:H/?key=AIzaSyCfXHtG7ylyNenz8ncsqAuS4njElL2dm68"
 
 
 export default function NextCity() {
     const [nextCityData, setNextCityData] = useState(null);
-
+    let imagevalue=''
     useEffect(() => {
         fetch(urlNextcity).then(x => x.json()).then(x => {
             let data = {
@@ -22,17 +22,26 @@ export default function NextCity() {
             data.country=x.values[1][1]
             data.place=x.values[1][2]
             data.city_accent=x.values[1][5]
-
+            data.url=x.values[1][6]
+            
             setNextCityData(data)
         })
     }, []);
-    
-    
+    if (nextCityData?.url && nextCityData.url.startsWith("https://"))
+    {
+      imagevalue=nextCityData?.url
+    }
+    else
+    {
+      imagevalue='../img/Etapes/'+ nextCityData?.url +'.jpg'
+    }
+    console.log(nextCityData)
     return <>
           <Card sx={{ maxWidth: 345 }}>
               <CardMedia
                 component="img"
-                image={'../img/Etapes/'+ nextCityData?.city +'.jpg'}
+                
+                image={imagevalue}
                 alt={nextCityData?.city_accent}
                 sx={{
                   height: 200,
